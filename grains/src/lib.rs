0a86ccbdcbeaab85e49fbd52d4ac8a7e0dfbc03d
Victor H. Faria <victorfaria1590@gmail.com>
2023-03-12 20:52:04 -0300

    grains solution v2

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,26 +1,13 @@
-pub fn validate_square(s: u32) -> () 
+pub fn square(s: u32) -> u64 
 {
     if s < 1 || s > 64 
     {
         panic!("Square must be between 1 and 64");
     }
-}
-
-pub fn square(s: u32) -> u64 
-{
-    validate_square(s);
-    let base : u64 = 2;
-    let grains : u64 = base.pow(s - 1);
-    grains
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 
 {
-    let mut sum : u64 = 1;
-    let base : u64 = 2;
-    for i in 1..64
-    {
-        sum += base.pow(i) as u64;
-    }
-    sum
+    u64::MAX
 }
